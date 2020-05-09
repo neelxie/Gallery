5291743c181c74d8617314ec67075f47967da3aa
Derrick Sekidde <kidricederek@gmail.com>
2020-05-09 22:53:52 +0300

    Good things happen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-// import * as serviceWorker from './serviceWorker';
 import { saveState } from './localStorage';
 import Routes from './App';
 import './index.css';
